1b0b879e0878ede8e4f3220bbce63746f62427d6
Nicolas Spiric <nspiric@nspx.ca>
2015-09-28 15:10:50 -0400

    updated tests

--- tests/tests.js
+++ tests/tests.js
@@ -4,26 +4,22 @@ var waterfall = require('run-waterfall')
 var rcinfo = require('..')
 
 var fileInfo = {
+  AuthorName: 'Ted Peck',
   FileVersion: '1.0.0.1',
   ProductVersion: '1.0.0.1',
   ProductName: 'ShowVer',
   InternalName: 'ShowVer',
-  LegalCopyright: 'Copyright © 2002'
 }
-
 
 function createFileInfo(winFile) {
   return function (t) {
 
     waterfall([
       function (callback) {
-        rcinfo(winFile, function(error, info){
-          if (!error) {
-            t.true(info, 'should return an initialised object')
-          }  
-        })  
-        
-        callback()      
+        rcinfo(winFile, callback)
+      }, function(info, callback){
+        t.ok(info, 'should return an initialised object')
+        callback()
       }
     ], function (err) {
       t.end(err)
@@ -39,7 +35,7 @@ function fileInfoTest(winFile, key, expectedValue) {
       function (callback) {
         rcinfo(winFile, callback)
       }, function (info, callback) {		
-		    t.equal(info[key], expectedValue)
+		    t.equal(info[key], expectedValue, key + ' should be equal to expected value: ' + expectedValue)
 		    callback()
       }
     ], function (err) {
@@ -53,11 +49,14 @@ if (process.platform !== 'win32') {
   console.log('Platform is ' + process.platform + ' skipping win32 tests...')  
 } else {
   console.log('Executing win32 tests...')
-  test('file info creation', createFileInfo('./bin/ShowVer.exe'))
   
-  test('file version test', fileInfoTest('./bin/ShowVer.exe', fileInfo.FileVersion))
-  test('product version test', fileInfoTest('./bin/ShowVer.exe', fileInfo.ProductVersion))
-  test('product name test', fileInfoTest('./bin/ShowVer.exe', fileInfo.ProductName))
-  test('internal name test', fileInfoTest('./bin/ShowVer.exe', fileInfo.InternalName))
-  test('legal copyright test', fileInfoTest('./bin/ShowVer.exe', fileInfo.LegalCopyright))
+  var executable = path.resolve(__dirname, '..', 'bin', 'ShowVer.exe');
+  
+  test('file info creation', createFileInfo(executable))
+  
+  test('Author name test', fileInfoTest(executable, 'AuthorName', fileInfo.AuthorName))
+  test('file version test', fileInfoTest(executable, 'FileVersion', fileInfo.FileVersion))
+  test('product version test', fileInfoTest(executable, 'ProductVersion', fileInfo.ProductVersion))
+  test('product name test', fileInfoTest(executable, 'ProductName', fileInfo.ProductName))
+  test('internal name test', fileInfoTest(executable, 'InternalName', fileInfo.InternalName))  
 }
